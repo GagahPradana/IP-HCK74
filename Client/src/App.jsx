36cf8f9b27b74935b130f4aa7943d57b882350ce
GagahPradana <gagahpradanap@gmail.com>
2024-09-04 20:45:45 +0700

    get data from 3rd party api TMDB

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,8 +2,22 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/Footer";
 import MobileNav from "./components/MobileNav";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  async function fecthTrendingData() {
+    try {
+      const response = await axios.get("/trending/all/day");
+      console.log("response", response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fecthTrendingData();
+  }, []);
+
   return (
     <>
       <main className="pb-14 lg:pb-0">
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
+import axios from "axios";
 
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.headers.common["Authorization"] = `Bearer ${
+  import.meta.env.VITE_ACCESS_TOKEN
+}`;
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
